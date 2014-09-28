a95350db407f799d18296d93c2e2adc527dbd038
SorryForYourLoss <sturninja@gmail.com>
2014-09-28 19:50:29 -0400

    fix custom avatars

--- config/config-mod.js
+++ config/config-mod.js
@@ -2,6 +2,7 @@
  * This file represents what changes are made into config/config.js
  * Not the complete file, just minor changes that can be modified
  * Basics that have to added to are here!
+ *
  */
 
  exports.consoleips = ['127.0.0.1'];
